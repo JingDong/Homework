b8e78afd04bd9e7c6e2e0cd3ae2b2e7e4e993f1b
Jing Dong <donjing@gmail.com>
2021-07-13 16:24:13 -0700

    Initial commit

---
+++ src/com/Bank/BankTransaction.java
@@ -0,0 +1,9 @@
+package com.Bank;
+
+public class BankTransaction {
+    private int accountNumber;
+    private float balance;
+    private String customerName;
+    private String emailAddress;
+    private String phoneNumber;
+}
