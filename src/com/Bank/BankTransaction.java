d0bae78000f30afc822c07c87b33487e78d103ab
jingdong <donjing@gmail.com>
2021-07-13 16:54:35 -0700

    Finished bank transaction assignment

--- src/com/Bank/BankTransaction.java
+++ src/com/Bank/BankTransaction.java
@@ -1,9 +1,74 @@
 package com.Bank;
 
 public class BankTransaction {
-    private int accountNumber;
+    private String accountNumber;
     private float balance;
     private String customerName;
     private String emailAddress;
     private String phoneNumber;
+
+
+    BankTransaction(){}
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void deposit(float amount){
+        this.balance += amount;
+        System.out.println(this.customerName + "balance: " + this.balance);
+    }
+    public void withdraw(float amount){
+        if(this.balance > amount){
+            this.balance -= amount;
+            System.out.println(this.customerName + "balance: " + this.balance);
+        }
+        else{
+            System.out.println("Insufficient amount. Cannot withdraw." );
+        }
+    }
+    public void customerInfo(){
+        System.out.println("Customer Name: " + this.customerName);
+        System.out.println("Account Number: " + this.accountNumber);
+        System.out.println("Balance: " + this.balance);
+        System.out.println("Email: " + this.emailAddress);
+        System.out.println("Phone Number:" + this.phoneNumber);
+
+    }
+
 }
--- src/com/Bank/Main.java
+++ src/com/Bank/Main.java
@@ -1,8 +1,49 @@
 package com.Bank;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        // create bank account for Julia Roberts
+        BankTransaction transaction = new BankTransaction();
+        transaction.setAccountNumber("12345");
+        transaction.setCustomerName("Julia Roberts");
+        transaction.setEmailAddress("juliaRoberts@gmail.com");
+        transaction.setPhoneNumber("333-444-5556");
+        transaction.setBalance(1000);
+
+        Scanner sc = new Scanner(System.in);
+        float amount;
+        while(true){
+            System.out.println("Please select from the menu:");
+            System.out.println("1. Show Account Balance");
+            System.out.println("2. Deposit Money");
+            System.out.println("3. Withdraw Money");
+            System.out.println("4. Exit");
+            int option = sc.nextInt();
+            if(option == 1){
+                System.out.println("Balance: " + transaction.getBalance());
+            }
+            else if(option == 2){
+                System.out.println("Please input the amount for deposit:");
+                amount = sc.nextFloat();
+                transaction.deposit(amount);
+            }
+            else if(option == 3){
+                System.out.println("Please input the amount for withdraw:");
+                amount = sc.nextFloat();
+                transaction.withdraw(amount);
+            }
+
+            else if(option == 4){
+                break;
+            }
+            else{
+                System.out.println("Please select 1-4 from Menu");
+            }
+        }
+
     }
 }
